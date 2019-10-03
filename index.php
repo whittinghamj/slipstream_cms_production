883df61929c8ca1105b0631ae8f39fbed8e25036
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-03 23:08:40 +0100

    no message

--- index.php
+++ index.php
@@ -82,15 +82,13 @@ include('inc/global_vars.php');
                 </form>
 
                 <div class="social-auth-links text-center">
-                    <p>- OR -</p>
                     <!--
                     <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
                     <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
                     -->
-                    <a href="https://clients.deltacolo.com/cart.php?a=add&pid=62" class="btn btn-block btn-success btn-flat">Order Now</a>
                 </div>
 
-                <a href="https://clients.deltacolo.com/pwreset.php">I forgot my password</a>
+                <!-- <a href="https://clients.deltacolo.com/pwreset.php">I forgot my password</a> -->
                 
             </div>
         </div>
