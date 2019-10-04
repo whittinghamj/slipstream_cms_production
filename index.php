4f8208a0bfd5fcce3e44ffd5356d7787e787b1be
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-04 10:18:34 +0100

    no message

--- index.php
+++ index.php
@@ -54,87 +54,82 @@ include('inc/global_vars.php');
 </head>
 
 <body class="hold-transition login-page">
-    <div class="col-lg-6">
-        <div class="login-box">
-            <div class="login-logo">
+    <div class="row">
+        <div class="col-lg-12">
+            <center>
                 <b><?php echo $site['title']; ?></b>
-                <!-- <img src="assets/images/cover.pngso" alt="" width="300px"> -->
-            </div>
-            <div id="login_form">
-                <div class="login-box-body">
-                    <p class="login-box-msg">Sign in</p>
-                    
-                    <div id="status_message"></div>
-                    
-                    <form action="login.php" method="post">
-                        <div class="form-group has-feedback">
-                            <input type="text" class="form-control" placeholder="username" name="username" id="username">
-                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                        </div>
-                        <div class="form-group has-feedback">
-                            <input type="password" class="form-control" placeholder="********" name="password" id="password">
-                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <button type="submit" class="btn btn-block btn-primary btn-flat full-width">Sign In</button>
+            </center>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="login-box">
+                <center>
+                    <h4>Admin Login</h4>
+                </center>
+                <div id="login_form">
+                    <div class="login-box-body">
+                        <p class="login-box-msg">Sign in</p>
+                        
+                        <div id="status_message"></div>
+                        
+                        <form action="login.php" method="post">
+                            <div class="form-group has-feedback">
+                                <input type="text" class="form-control" placeholder="username" name="username" id="username">
+                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                             </div>
-                        </div>
-                    </form>
-
-                    <div class="social-auth-links text-center">
-                        <!--
-                        <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
-                        <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
-                        -->
-                    </div>
-
-                    <!-- <a href="https://clients.deltacolo.com/pwreset.php">I forgot my password</a> -->
-                    
-                </div>
-            </div>
-
-            <div id="register_form" style="display: none">
-                <div class="register-box-body">
-                    <p class="login-box-msg">Register</p>
-                    <form action="../../index.html" method="post">
-                        <div class="form-group has-feedback">
-                            <input type="text" class="form-control" placeholder="Full name">
-                            <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                        </div>
-                        <div class="form-group has-feedback">
-                            <input type="email" class="form-control" placeholder="Email">
-                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                        </div>
-                        <div class="form-group has-feedback">
-                            <input type="password" class="form-control" placeholder="Password">
-                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                        </div>
-                        <div class="form-group has-feedback">
-                            <input type="password" class="form-control" placeholder="Retype password">
-                            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-8">
-                                <div class="checkbox icheck">
-                                    <label>
-                                        <input type="checkbox"> I agree to the <a href="#">terms</a>
-                                    </label>
+                            <div class="form-group has-feedback">
+                                <input type="password" class="form-control" placeholder="********" name="password" id="password">
+                                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <button type="submit" class="btn btn-block btn-primary btn-flat full-width">Sign In</button>
                                 </div>
                             </div>
-                            <!-- /.col -->
-                            <div class="col-xs-4">
-                                <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
-                            </div>
-                            <!-- /.col -->
-                        </div>
-                    </form>
+                        </form>
 
-                    <a href="#" onclick="switch_content('login');" class="text-center">I already have a membership</a>
+                        <div class="social-auth-links text-center"></div>                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6">
+            <div class="login-box">
+                <center>
+                    <h4>Reseller Login</h4>
+                </center>
+                <div id="login_form">
+                    <div class="login-box-body">
+                        <p class="login-box-msg">Sign in</p>
+                        
+                        <div id="status_message"></div>
+                        
+                        <form action="login.php" method="post">
+                            <div class="form-group has-feedback">
+                                <input type="text" class="form-control" placeholder="username" name="username" id="username">
+                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                            </div>
+                            <div class="form-group has-feedback">
+                                <input type="password" class="form-control" placeholder="********" name="password" id="password">
+                                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <button type="submit" class="btn btn-block btn-primary btn-flat full-width">Sign In</button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <div class="social-auth-links text-center"></div>                    
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
     <!-- jQuery 2.1.4 -->
     <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
     <!-- Bootstrap 3.3.5 -->
