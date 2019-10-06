6035035176de00b5c01122e2300d96cc55f1ade5
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-06 14:21:17 +0100

    no message

--- customers/index.php
+++ customers/index.php
@@ -30,8 +30,9 @@ if(empty($type)) {
 	die('missing type');
 }
 
-$query = $conn->query("SELECT * FROM `customers` WHERE `username` = '".$username."' AND `password` = '".$password."' ");
-$customer = $query->fetch(PDO::FETCH_ASSOC);
+$query 					= $conn->query("SELECT * FROM `customers` WHERE `username` = '".$username."' AND `password` = '".$password."' ");
+$customer 				= $query->fetch(PDO::FETCH_ASSOC);
+$customer['bouquet']	= explode(",", $customer['bouquet']);
 
 if(empty($customer)) {
 	die("customer not found");
@@ -377,6 +378,116 @@ if($type == 'flussonic') {
 		print "#SERVICE 4097:0:1:0:0:0:0:0:0:0:http%3A//".$global_settings['cms_access_url_raw'].":".$global_settings['cms_port']."/customer_streams/".$customer['username']."/".$customer['password']."/".$stream['server_id']."/".$stream['id'].'.ts'.$new_line;
 		print "#DESCRIPTION ".stripslashes($stream['name']).$new_line;
 	}
+}elseif($type == 'dev') {
+	//Generate text file on the fly
+	//header("Content-type: text/plain");
+	//header("Content-Disposition: attachment; filename=playlist.m3u");
+
+	$new_line = "\n";
+
+	// demo m3u format
+	// #EXTM3U
+	// #EXTINF:-1,CHANNEL NAME
+	// http://link.to.stream
+
+	// print "#EXTM3U".$new_line;
+
+	// build live tv list
+	foreach($customer['bouquet'] as $bouquet){
+		// get streams for this bouquet
+		$query 						= $conn->query("SELECT `streams` FROM `bouquets` WHERE `id` = '".$bouquet['id']."'");
+		$temp_bouquet 				= $query->fetch(PDO::FETCH_ASSOC);
+		$temp_bouquet['streams'] 	= explode(",", $temp_bouquet['streams'])
+
+		// add each stream into a master bouquet
+		foreach($temp_bouquet['streams'] as $stream){
+			$master_bouquet[] = $stream['id'];
+		}
+	}
+
+
+	debug($master_bouquet);
+
+
+
+
+
+	/*
+	$query = $conn->query("SELECT `id`,`server_id`,`name`,`status`,`source_type` FROM `streams` WHERE `stream_type` = 'output' AND `user_id` = '".$customer['user_id']."' ORDER BY `name` ASC");
+	$streams = $query->fetchAll(PDO::FETCH_ASSOC);
+
+	foreach($streams as $stream) {
+
+		if($stream['source_type'] == 'hidden'){
+			$stream['source_type'] = '';
+		}else{
+			// $stream['source_type'] = ' ('.strtoupper($stream['source_type']).') ';
+			$stream['source_type'] = '';
+		}
+
+		if($stream['status'] == 'online'){
+			print '#EXTINF:-1,'.stripslashes($stream['name']).$stream['source_type'].''.$new_line;
+		}else{
+			print '#EXTINF:-1,'.stripslashes($stream['name']).$stream['source_type'].'['.strtoupper($stream['status']).']'.$new_line;
+		}
+		print "http://".$global_settings['cms_access_url_raw'].":".$global_settings['cms_port']."/customer_streams/".$customer['username']."/".$customer['password']."/".$stream['server_id']."/".$stream['id'].$new_line;
+	}
+	*/
+	
+	// build channel list
+	/*
+	if($customer['channel_content'] == 'on'){
+		$query = $conn->query("SELECT * FROM `channels` WHERE `enable` = 'yes' AND `user_id` = '".$customer['user_id']."' ORDER BY `name` ASC");
+		$channels = $query->fetchAll(PDO::FETCH_ASSOC);
+
+		foreach($channels as $channel) {
+			print '#EXTINF:-1,'.stripslashes($channel['name']).$new_line;
+			print "http://".$global_settings['cms_access_url_raw'].":".$global_settings['cms_port']."/customer_channels/".$customer['username']."/".$customer['password']."/".$channel['server_id']."/".$channel['id'].$new_line;
+		}
+	}
+
+	// build series list
+	if($customer['vod_content'] == 'on'){
+		$query = $conn->query("SELECT * FROM `tv_series` WHERE `user_id` = '".$customer['user_id']."' ORDER BY `name` ASC");
+		$tv_series = $query->fetchAll(PDO::FETCH_ASSOC);
+
+		$total_tv_series = count($tv_series);
+
+		if($total_tv_series > 0){
+			$tv_series_count = 0;
+			foreach($tv_series as $series) {
+
+				$query = $conn->query("SELECT * FROM `tv_series_files` WHERE `tv_series_id` = '".$series['id']."' ORDER BY `order` ");
+				$series_files = $query->fetchAll(PDO::FETCH_ASSOC);
+
+				foreach($series_files as $series_file) {
+					print '#EXTINF:-1,'.stripslashes($series_file['name']).$new_line;
+					print "http://".$global_settings['cms_access_url_raw'].":".$global_settings['cms_port']."/customer_series/".$customer['username']."/".$customer['password']."/".$series['server_id']."/".$series_file['id'].$new_line;
+				}
+			}
+		}
+
+		// build vod list
+		$query = $conn->query("SELECT * FROM `vod` WHERE `user_id` = '".$customer['user_id']."' ORDER BY `name` ASC");
+		$vods = $query->fetchAll(PDO::FETCH_ASSOC);
+
+		$total_vods = count($vods);
+
+		if($total_vods > 0){
+			foreach($vods as $vod) {
+
+				if(empty($vod['cover_photo'])) {
+					$logo = 'http://slipstreamiptv.com/img/iptv_stream_icon.png';
+				}else{
+					$logo = stripslashes($vod['cover_photo']);
+				}
+
+				print '#EXTINF:-1,'.stripslashes($vod['name']).$new_line;
+				print "http://".$global_settings['cms_access_url_raw'].":".$global_settings['cms_port']."/customer_vod/".$customer['username']."/".$customer['password']."/".$vod['server_id']."/".$vod['id'].$new_line;
+			}
+		}
+	}
+	*/
 }else{
 	echo "unknown playlist type";
 }
