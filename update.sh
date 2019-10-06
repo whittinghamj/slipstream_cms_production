d0adcc732b76e0fd7ed31f47e3a9be0b9a4d37c9
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-06 18:53:40 +0100

    no message

--- update.sh
+++ update.sh
@@ -5,3 +5,10 @@ cd /var/www/html/portal && git pull git@github.com:whittinghamj/slipstream_cms_s
 
 # confirm folder permissions
 chmod 777 /var/www/html/portal/m3u_uploads
+
+# mysql status check
+UP=$(pgrep mysql | wc -l);
+if [ "$UP" -ne 1 ];
+then
+    sudo service mysql start
+fi
