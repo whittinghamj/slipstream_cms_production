668b7341060499a41dd359289afcbce2495e7377
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-06 18:27:07 +0100

    no message

--- update.sh
+++ update.sh
@@ -2,3 +2,6 @@
 
 # bash git update script
 cd /var/www/html/portal && git pull git@github.com:whittinghamj/slipstream_cms_server.git
+
+# confirm folder permissions
+chmod 777 /var/www/html/portal/m3u_uploads
