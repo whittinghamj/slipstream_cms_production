49befc208aef6d699f9e913c9b574e5d5e3df587
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-07 01:42:09 +0100

    no message

--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,30 @@
 <?php
 
+function encrypt($string, $key=5)
+{
+    $result = '';
+    for($i=0, $k= strlen($string); $i<$k; $i++) {
+        $char = substr($string, $i, 1);
+        $keychar = substr($key, ($i % strlen($key))-1, 1);
+        $char = chr(ord($char)+ord($keychar));
+        $result .= $char;
+    }
+    return base64_encode($result);
+}
+
+function decrypt($string, $key=5)
+{
+    $result = '';
+    $string = base64_decode($string);
+    for($i=0,$k=strlen($string); $i< $k ; $i++) {
+        $char = substr($string, $i, 1);
+        $keychar = substr($key, ($i % strlen($key))-1, 1);
+        $char = chr(ord($char)-ord($keychar));
+        $result.=$char;
+    }
+    return $result;
+}
+
 function is_customer_connection_allowed($customer_id){
     global $conn;
 
