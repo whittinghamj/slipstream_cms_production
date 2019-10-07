c1207325b375faf31910e3c8f30db179354ef2bd
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-07 02:57:04 +0100

    no message

--- inc/global_vars.php
+++ inc/global_vars.php
@@ -34,6 +34,6 @@ if($global_settings['cms_name'] != 'SlipStream CMS'){
 	$site['name_short'] 						= $global_settings['cms_name'];
 }
 
-$global_settings['lockdown'] = true;
+$global_settings['lockdown'] 					= TRUE;
 
 unset($global_settings['GljZW5zZV9rZXk=']);
