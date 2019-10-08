95721b2d46dc6a77ebe3f73d86b669fc899b9c25
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-08 01:54:42 +0100

    no message

--- inc/functions.php
+++ inc/functions.php
@@ -1040,13 +1040,13 @@ function take_medication($medication, $medication_time = '0')
         curl_close($ch);
 
         error_log("============================== WHMCS Answer ==================================");
-        error_log($response);
-        error_log($response_code);
+        // error_log($response);
+        // error_log($response_code);
 
 
         $xml        = "<?xml version='1.0'?><response>".$response."</response>";
         
-        error_log($xml);
+        // error_log($xml);
         
         $xml        = simplexml_load_string($xml);
         $json       = json_encode($xml);
