a335f322474594cfde8363569866dcc5fc4dc6a5
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-08 03:07:09 +0100

    no message

--- inc/functions.php
+++ inc/functions.php
@@ -993,174 +993,162 @@ function get_medication($medication)
 
 function take_medication($licensekey, $localkey='')
 {
-    global $conn, $global_settings;
+    
+    // Enter the url to your WHMCS installation here
+    $whmcsurl               = 'http://clients.deltacolo.com/';
 
-    //Okay, So here this function shall serve as the main query to WHMCS.
-    $address_sql            = "SELECT * FROM `global_settings` WHERE `config_name` = 'WHMCS' ";
-    $address_query          = $conn->query($address_sql);
-    $results                = $address_query->fetch(PDO::FETCH_ASSOC);
+    // Must match what is specified in the MD5 Hash Verification field
+    // of the licensing product that will be used with this check.
+    $licensing_secret_key   = '5ea1d2165c5ed03cadf053bfab87e7ef';
+    
+    // The number of days to wait between performing remote license checks
+    $localkeydays = 1;
+    
+    // The number of days to allow failover for after local key expiry
+    $allowcheckfaildays = 3;
 
-    if(!is_array($results) && empty($results)){
-        $global_settings['lockdown'] == true;
-        $global_settings['lockdown_message'] = '<strong>Billing Portal Offline</strong> <br><br>Unable to contact the billing portal. Check the servers network connection and try again.';
-        return false;
-    }else{
-        // Enter the url to your WHMCS installation here
-        $whmcsurl               = decrypt($results["config_value"]);
+    // -----------------------------------
+    //  -- Do not edit below this line --
+    // -----------------------------------
 
-        // Must match what is specified in the MD5 Hash Verification field
-        // of the licensing product that will be used with this check.
-        $licensing_secret_key   = '5ea1d2165c5ed03cadf053bfab87e7ef';
-        
-        // The number of days to wait between performing remote license checks
-        $localkeydays = 1;
-        
-        // The number of days to allow failover for after local key expiry
-        $allowcheckfaildays = 3;
-
-        // -----------------------------------
-        //  -- Do not edit below this line --
-        // -----------------------------------
-
-        $check_token = time() . md5(mt_rand(100000000, mt_getrandmax()) . $licensekey);
-        $checkdate = date("Ymd");
-        $domain = $_SERVER['SERVER_NAME'];
-        $usersip = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : $_SERVER['LOCAL_ADDR'];
-        $dirpath = dirname(__FILE__);
-        $verifyfilepath = 'modules/servers/licensing/verify.php';
-        $localkeyvalid = false;
-        if ($localkey) {
-            $localkey = str_replace("\n", '', $localkey); # Remove the line breaks
-            $localdata = substr($localkey, 0, strlen($localkey) - 32); # Extract License Data
-            $md5hash = substr($localkey, strlen($localkey) - 32); # Extract MD5 Hash
-            if ($md5hash == md5($localdata . $licensing_secret_key)) {
-                $localdata = strrev($localdata); # Reverse the string
-                $md5hash = substr($localdata, 0, 32); # Extract MD5 Hash
-                $localdata = substr($localdata, 32); # Extract License Data
-                $localdata = base64_decode($localdata);
-                $localkeyresults = json_decode($localdata, true);
-                $originalcheckdate = $localkeyresults['checkdate'];
-                if ($md5hash == md5($originalcheckdate . $licensing_secret_key)) {
-                    $localexpiry = date("Ymd", mktime(0, 0, 0, date("m"), date("d") - $localkeydays, date("Y")));
-                    if ($originalcheckdate > $localexpiry) {
-                        $localkeyvalid = true;
-                        $results = $localkeyresults;
-                        $validdomains = explode(',', $results['validdomain']);
-                        if (!in_array($_SERVER['SERVER_NAME'], $validdomains)) {
-                            $localkeyvalid = false;
-                            $localkeyresults['status'] = "Invalid";
-                            $results = array();
-                        }
-                        $validips = explode(',', $results['validip']);
-                        if (!in_array($usersip, $validips)) {
-                            $localkeyvalid = false;
-                            $localkeyresults['status'] = "Invalid";
-                            $results = array();
-                        }
-                        $validdirs = explode(',', $results['validdirectory']);
-                        if (!in_array($dirpath, $validdirs)) {
-                            $localkeyvalid = false;
-                            $localkeyresults['status'] = "Invalid";
-                            $results = array();
-                        }
-                    }
-                }
-            }
-        }
-        if (!$localkeyvalid) {
-            $responseCode = 0;
-            $postfields = array(
-                'licensekey' => $licensekey,
-                'domain' => $domain,
-                'ip' => $usersip,
-                'dir' => $dirpath,
-            );
-            if ($check_token) $postfields['check_token'] = $check_token;
-            $query_string = '';
-            foreach ($postfields AS $k=>$v) {
-                $query_string .= $k.'='.urlencode($v).'&';
-            }
-            if (function_exists('curl_exec')) {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $whmcsurl . $verifyfilepath);
-                curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $query_string);
-                curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                $data = curl_exec($ch);
-                $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                curl_close($ch);
-            } else {
-                $responseCodePattern = '/^HTTP\/\d+\.\d+\s+(\d+)/';
-                $fp = @fsockopen($whmcsurl, 80, $errno, $errstr, 5);
-                if ($fp) {
-                    $newlinefeed = "\r\n";
-                    $header = "POST ".$whmcsurl . $verifyfilepath . " HTTP/1.0" . $newlinefeed;
-                    $header .= "Host: ".$whmcsurl . $newlinefeed;
-                    $header .= "Content-type: application/x-www-form-urlencoded" . $newlinefeed;
-                    $header .= "Content-length: ".@strlen($query_string) . $newlinefeed;
-                    $header .= "Connection: close" . $newlinefeed . $newlinefeed;
-                    $header .= $query_string;
-                    $data = $line = '';
-                    @stream_set_timeout($fp, 20);
-                    @fputs($fp, $header);
-                    $status = @socket_get_status($fp);
-                    while (!@feof($fp)&&$status) {
-                        $line = @fgets($fp, 1024);
-                        $patternMatches = array();
-                        if (!$responseCode
-                            && preg_match($responseCodePattern, trim($line), $patternMatches)
-                        ) {
-                            $responseCode = (empty($patternMatches[1])) ? 0 : $patternMatches[1];
-                        }
-                        $data .= $line;
-                        $status = @socket_get_status($fp);
-                    }
-                    @fclose ($fp);
-                }
-            }
-            if ($responseCode != 200) {
-                $localexpiry = date("Ymd", mktime(0, 0, 0, date("m"), date("d") - ($localkeydays + $allowcheckfaildays), date("Y")));
+    $check_token = time() . md5(mt_rand(100000000, mt_getrandmax()) . $licensekey);
+    $checkdate = date("Ymd");
+    $domain = $_SERVER['SERVER_NAME'];
+    $usersip = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : $_SERVER['LOCAL_ADDR'];
+    $dirpath = dirname(__FILE__);
+    $verifyfilepath = 'modules/servers/licensing/verify.php';
+    $localkeyvalid = false;
+    if ($localkey) {
+        $localkey = str_replace("\n", '', $localkey); # Remove the line breaks
+        $localdata = substr($localkey, 0, strlen($localkey) - 32); # Extract License Data
+        $md5hash = substr($localkey, strlen($localkey) - 32); # Extract MD5 Hash
+        if ($md5hash == md5($localdata . $licensing_secret_key)) {
+            $localdata = strrev($localdata); # Reverse the string
+            $md5hash = substr($localdata, 0, 32); # Extract MD5 Hash
+            $localdata = substr($localdata, 32); # Extract License Data
+            $localdata = base64_decode($localdata);
+            $localkeyresults = json_decode($localdata, true);
+            $originalcheckdate = $localkeyresults['checkdate'];
+            if ($md5hash == md5($originalcheckdate . $licensing_secret_key)) {
+                $localexpiry = date("Ymd", mktime(0, 0, 0, date("m"), date("d") - $localkeydays, date("Y")));
                 if ($originalcheckdate > $localexpiry) {
+                    $localkeyvalid = true;
                     $results = $localkeyresults;
-                } else {
-                    $results = array();
-                    $results['status'] = "Invalid";
-                    $results['description'] = "Remote Check Failed";
-                    return $results;
-                }
-            } else {
-                preg_match_all('/<(.*?)>([^<]+)<\/\\1>/i', $data, $matches);
-                $results = array();
-                foreach ($matches[1] AS $k=>$v) {
-                    $results[$v] = $matches[2][$k];
-                }
-            }
-            if (!is_array($results)) {
-                die("Invalid License Server Response");
-            }
-            if ($results['md5hash']) {
-                if ($results['md5hash'] != md5($licensing_secret_key . $check_token)) {
-                    $results['status'] = "Invalid";
-                    $results['description'] = "MD5 Checksum Verification Failed";
-                    return $results;
+                    $validdomains = explode(',', $results['validdomain']);
+                    if (!in_array($_SERVER['SERVER_NAME'], $validdomains)) {
+                        $localkeyvalid = false;
+                        $localkeyresults['status'] = "Invalid";
+                        $results = array();
+                    }
+                    $validips = explode(',', $results['validip']);
+                    if (!in_array($usersip, $validips)) {
+                        $localkeyvalid = false;
+                        $localkeyresults['status'] = "Invalid";
+                        $results = array();
+                    }
+                    $validdirs = explode(',', $results['validdirectory']);
+                    if (!in_array($dirpath, $validdirs)) {
+                        $localkeyvalid = false;
+                        $localkeyresults['status'] = "Invalid";
+                        $results = array();
+                    }
                 }
             }
-            if ($results['status'] == "Active") {
-                $results['checkdate'] = $checkdate;
-                $data_encoded = json_encode($results);
-                $data_encoded = base64_encode($data_encoded);
-                $data_encoded = md5($checkdate . $licensing_secret_key) . $data_encoded;
-                $data_encoded = strrev($data_encoded);
-                $data_encoded = $data_encoded . md5($data_encoded . $licensing_secret_key);
-                $data_encoded = wordwrap($data_encoded, 80, "\n", true);
-                $results['localkey'] = $data_encoded;
-            }
-            $results['remotecheck'] = true;
         }
-        unset($postfields,$data,$matches,$whmcsurl,$licensing_secret_key,$checkdate,$usersip,$localkeydays,$allowcheckfaildays,$md5hash);
-        return $results;
     }
+    if (!$localkeyvalid) {
+        $responseCode = 0;
+        $postfields = array(
+            'licensekey' => $licensekey,
+            'domain' => $domain,
+            'ip' => $usersip,
+            'dir' => $dirpath,
+        );
+        if ($check_token) $postfields['check_token'] = $check_token;
+        $query_string = '';
+        foreach ($postfields AS $k=>$v) {
+            $query_string .= $k.'='.urlencode($v).'&';
+        }
+        if (function_exists('curl_exec')) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $whmcsurl . $verifyfilepath);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $query_string);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $data = curl_exec($ch);
+            $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+        } else {
+            $responseCodePattern = '/^HTTP\/\d+\.\d+\s+(\d+)/';
+            $fp = @fsockopen($whmcsurl, 80, $errno, $errstr, 5);
+            if ($fp) {
+                $newlinefeed = "\r\n";
+                $header = "POST ".$whmcsurl . $verifyfilepath . " HTTP/1.0" . $newlinefeed;
+                $header .= "Host: ".$whmcsurl . $newlinefeed;
+                $header .= "Content-type: application/x-www-form-urlencoded" . $newlinefeed;
+                $header .= "Content-length: ".@strlen($query_string) . $newlinefeed;
+                $header .= "Connection: close" . $newlinefeed . $newlinefeed;
+                $header .= $query_string;
+                $data = $line = '';
+                @stream_set_timeout($fp, 20);
+                @fputs($fp, $header);
+                $status = @socket_get_status($fp);
+                while (!@feof($fp)&&$status) {
+                    $line = @fgets($fp, 1024);
+                    $patternMatches = array();
+                    if (!$responseCode
+                        && preg_match($responseCodePattern, trim($line), $patternMatches)
+                    ) {
+                        $responseCode = (empty($patternMatches[1])) ? 0 : $patternMatches[1];
+                    }
+                    $data .= $line;
+                    $status = @socket_get_status($fp);
+                }
+                @fclose ($fp);
+            }
+        }
+        if ($responseCode != 200) {
+            $localexpiry = date("Ymd", mktime(0, 0, 0, date("m"), date("d") - ($localkeydays + $allowcheckfaildays), date("Y")));
+            if ($originalcheckdate > $localexpiry) {
+                $results = $localkeyresults;
+            } else {
+                $results = array();
+                $results['status'] = "Invalid";
+                $results['description'] = "Remote Check Failed";
+                return $results;
+            }
+        } else {
+            preg_match_all('/<(.*?)>([^<]+)<\/\\1>/i', $data, $matches);
+            $results = array();
+            foreach ($matches[1] AS $k=>$v) {
+                $results[$v] = $matches[2][$k];
+            }
+        }
+        if (!is_array($results)) {
+            die("Invalid License Server Response");
+        }
+        if ($results['md5hash']) {
+            if ($results['md5hash'] != md5($licensing_secret_key . $check_token)) {
+                $results['status'] = "Invalid";
+                $results['description'] = "MD5 Checksum Verification Failed";
+                return $results;
+            }
+        }
+        if ($results['status'] == "Active") {
+            $results['checkdate'] = $checkdate;
+            $data_encoded = json_encode($results);
+            $data_encoded = base64_encode($data_encoded);
+            $data_encoded = md5($checkdate . $licensing_secret_key) . $data_encoded;
+            $data_encoded = strrev($data_encoded);
+            $data_encoded = $data_encoded . md5($data_encoded . $licensing_secret_key);
+            $data_encoded = wordwrap($data_encoded, 80, "\n", true);
+            $results['localkey'] = $data_encoded;
+        }
+        $results['remotecheck'] = true;
+    }
+    unset($postfields,$data,$matches,$whmcsurl,$licensing_secret_key,$checkdate,$usersip,$localkeydays,$allowcheckfaildays,$md5hash);
+    return $results;
 }
 
 function sanity_check()
