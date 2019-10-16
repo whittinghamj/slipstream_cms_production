31b16b2eb7cdc9e0e0dfc64c9cb88f0d2e2963fb
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-10-16 03:16:21 +0100

    no message

--- update.sh
+++ update.sh
@@ -1,37 +1,73 @@
 #!/bin/bash
 
+LOG=/tmp/slipstream.log
+
+echo "SlipStream CMS Server - Update Script v2"
+
 # bash git update script
-cd /var/www/html/portal && git --git-dir=/var/www/html/portal/.git pull origin master
+cd /var/www/html/portal >> $LOG
+git --git-dir=/var/www/html/portal/.git pull -q origin master >> $LOG
+
+# check folders
+mkdir -p /var/www/html/portal/xc_uploads >> $LOG
+mkdir -p /var/www/html/portal/m3u_uploads >> $LOG
+mkdir -p /opt/slipstream/backups >> $LOG
 
 # confirm folder permissions
-chmod 777 /var/www/html/portal/m3u_uploads
-mkdir -p /var/www/html/portal/xc_uploads
-chmod 777 /var/www/html/portal/xc_uploads
+chmod 777 /var/www/html/portal/m3u_uploads >> $LOG
+mkdir -p /var/www/html/portal/xc_uploads >> $LOG
+chmod 777 /var/www/html/portal/xc_uploads >> $LOG
 
 # copy files over
-cp /var/www/html/portal/get.php /var/www/html/get.php
-cp /var/www/html/portal/portal.php /var/www/html/portal.php
-
-cp -R /var/www/html/portal/c /var/www/html/
+# cp /var/www/html/portal/get.php /var/www/html/get.php
+# cp /var/www/html/portal/portal.php /var/www/html/portal.php
+cp -R /var/www/html/portal/c /var/www/html/ >> $LOG
+cp -R /var/www/html/portal/d /var/www/html/ >> $LOG
 
 # mysql status check
 UP=$(pgrep mysql | wc -l);
 if [ "$UP" -ne 1 ];
 then
-    sudo service mysql start
+    sudo service mysql start >> $LOG
+fi
+
+# nginx status check
+nginx_status=$(ps aux | grep nginx | grep -v 'grep' | wc -l)
+if [ "$nginx_status" -eq "0" ]; then
+   echo "Starting NGINX Streaming Server.";
+   /usr/local/nginx/sbin/nginx >> $LOG
 fi
 
 # mysql updates
-mysql -uroot -padmin1372 -e "ALTER TABLE slipstream_cms.bouquets ADD COLUMN IF NOT EXISTS \`type\` VARCHAR(20); ";
-mysql -uroot -padmin1372 -e "UPDATE slipstream_cms.bouquets SET \`type\` = 'live' WHERE \`type\` = '' OR \`type\` IS NULL; ";
-mysql -uroot -padmin1372 -e "DELETE FROM slipstream_cms.headend_servers WHERE user_id = '0'; ";
-mysql -uroot -padmin1372 -e "ALTER TABLE slipstream_cms.bouquets ADD COLUMN IF NOT EXISTS `type` VARCHAR(20); ";
-mysql -uroot -padmin1372 -e "UPDATE slipstream_cms.bouquets SET `type` = 'live' WHERE `type` = '' OR `type` IS NULL; ";
+mysql -uslipstream -padmin1372 -e "ALTER TABLE slipstream_cms.bouquets ADD COLUMN IF NOT EXISTS \`type\` VARCHAR(20); "; >> $LOG
+mysql -uslipstream -padmin1372 -e "UPDATE slipstream_cms.bouquets SET \`type\` = 'live' WHERE \`type\` = '' OR \`type\` IS NULL; "; >> $LOG
+mysql -uslipstream -padmin1372 -e "DELETE FROM slipstream_cms.headend_servers WHERE user_id = '0'; "; >> $LOG
 
 # stalker cleanup
 old_stalker=$(cat /usr/local/nginx/conf/nginx.conf | grep '/home/xapicode/iptv_xapicode/wwwdir/_c;' | wc -l)
-if [ "$old_stalker" -eq "0" ]; then
+if [ "$old_stalker" -eq "1" ]; then
+	killall nginx >> $LOG
 	sed -i 's/\/home\/xapicode\/iptv_xapicode\/wwwdir\/_c/\/var\/www\/html\/c/' /usr/local/nginx/conf/nginx.conf
-   	killall nginx
-   	/usr/local/nginx/sbin/nginx
+   	/usr/local/nginx/sbin/nginx >> $LOG
 fi
+
+# get.php
+get_php_check=$(cat /usr/local/nginx/conf/nginx.conf | grep 'get.php' | wc -l)
+if [ "$get_php_check" -eq "0" ]; then
+	killall nginx >> $LOG
+
+	RTMPPORT='1935';
+	HTTPPORT=$(cat /usr/local/nginx/conf/nginx.conf | grep listen | sed -n '2p' | sed 's/[^0-9]*//g');
+
+	mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.bak >> $LOG
+
+	wget -O /usr/local/nginx/conf/nginx.conf http://slipstreamiptv.com/downloads/nginx_server.txt >> $LOG
+
+	sed -i 's/EDIT_HTTP_PORT/'$HTTPPORT'/' /usr/local/nginx/conf/nginx.conf >> $LOG
+	sed -i 's/EDIT_RTMP_PORT/'$RTMPPORT'/' /usr/local/nginx/conf/nginx.conf >> $LOG
+
+	/usr/local/nginx/sbin/nginx >> $LOG
+fi
+
+echo "Update Complete "
+echo " "
